532cab65aab0db2c51f90371484edce315e192c6
Pratheek Rebala <pratheekrebala@live.com>
2014-03-24 15:00:59 -0400

    Main Script now returns all the 15269 executions serialised into a python list
    Removed print code with code to serialise all executions into a list
    
    Signed-off-by: Pratheek Rebala <pratheekrebala@live.com>

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 import ColumnMap
 import Mapping
+import Execution
 
 Maps = ColumnMap.Maps
 CrimeMappings = Mapping.CrimeMappings
+AllExecutions = []
 
 with open('08451-0001-Data.txt') as asciidata:
  for penalty in asciidata:
+ 	current_execution = Execution.Execution()
  	for column in Maps:
  		value = penalty[column['begin']:column['end']+1]
  		if value.isdigit():
  			if column['expand'] == True:
 	 			value = int(value)
  				if value == 0:
- 					print 'Missing Value'
+ 					attribute = 0
 	 			else:
-		 			print column['name'] + " : " + CrimeMappings[column['name']][value]
+		 			attribute = CrimeMappings[column['name']][value]
 
 		 	else:
-		 		print column['name'] + " : " + value
+		 		attribute = value
 		else:
-			if value == '':
-				print column['name'] + " : " + 'Missing Value'
-			else:
-				print column['name'] + " : " + value
+			attribute = value
+		setattr(current_execution,column['name'],attribute)
+	AllExecutions.append(current_execution.getExecution())
+
+print AllExecutions
