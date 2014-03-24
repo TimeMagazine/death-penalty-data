11add4658a557931b3765db1e67eedd8e4988a2f
Pratheek Rebala <pratheekrebala@live.com>
2014-03-24 13:51:01 -0400

    Fixed issues with Mapping Files
    Fixed syntax errors in ColumnMap.py and changed both the files to dicts to enable easier access in main script.
    
    Signed-off-by: Pratheek Rebala <pratheekrebala@live.com>

--- ColumnMap.py
+++ ColumnMap.py
@@ -1,22 +1,22 @@
-class ColumnMap(object):
-	ICPSRStudyNumber8451 = {1,4}
-	ICPSRSEditionNumber = {5,5}
-	ICPSRPartNumber = {6,6}
-	CaseNumber = {7,12}
-	RaceOfOffender = {13,13}
-	AgeAtExecution = {14,15}
-	NameOfOffender = {16,43}
-	PlaceOfExecution = {44,44}
-	JurisdictionOfExecution = {45,45}
-	CrimeCommitted = {46,47}
-	MethodOfExecution = {48,49}
-	DateDay = {50,51}
-	DateMonth = {52,53}
-	DateYear = {54,57}
-	CheckDigit = {58,59}
-	StateOfExecution = {60,61}
-	CountyOfConviction = {62,66}
-	ICPSRStateCode = {67,68}
-	SexOfOffender = {69,69}
-	CompensationCase = {70,70}
-	OccupationofOffender = {71,73}
+Maps = []
+Maps.append({'name' : 'ICPSRStudyNumber8451' , 'begin' : 0, 'end' : 3, 'expand' : False})
+Maps.append({'name' : 'ICPSRSEditionNumber' , 'begin' : 4, 'end' : 4, 'expand' : False})
+Maps.append({'name' : 'ICPSRPartNumber' , 'begin' : 5, 'end' : 5, 'expand' : False})
+Maps.append({'name' : 'CaseNumber' , 'begin' : 6, 'end' : 11, 'expand' : False})
+Maps.append({'name' : 'RaceOfOffender' , 'begin' : 12, 'end' : 12, 'expand' : True})
+Maps.append({'name' : 'AgeAtExecution' , 'begin' : 13, 'end' : 14, 'expand' : False})
+Maps.append({'name' : 'NameOfOffender' , 'begin' : 15, 'end' : 42, 'expand' : False})
+Maps.append({'name' : 'PlaceOfExecution' , 'begin' : 43, 'end' : 43, 'expand' : True})
+Maps.append({'name' : 'JurisdictionOfExecution' , 'begin' : 44, 'end' : 44, 'expand' : True})
+Maps.append({'name' : 'CrimeCommitted' , 'begin' : 45, 'end' : 46, 'expand' : True})
+Maps.append({'name' : 'MethodOfExecution' , 'begin' : 47, 'end' : 48, 'expand' : True})
+Maps.append({'name' : 'DateDay' , 'begin' : 49, 'end' : 50, 'expand' : False})
+Maps.append({'name' : 'DateMonth' , 'begin' : 51, 'end' : 52, 'expand' : False})
+Maps.append({'name' : 'DateYear' , 'begin' : 53, 'end' : 56, 'expand' : False})
+Maps.append({'name' : 'CheckDigit' , 'begin' : 57, 'end' : 58, 'expand' : False})
+Maps.append({'name' : 'StateOfExecution' , 'begin' : 59, 'end' : 60, 'expand' : True})
+Maps.append({'name' : 'CountyOfConviction' , 'begin' : 61, 'end' : 65, 'expand' : False})
+Maps.append({'name' : 'ICPSRStateCode' , 'begin' : 66, 'end' : 67, 'expand' : True})
+Maps.append({'name' : 'SexOfOffender' , 'begin' : 68, 'end' : 68, 'expand' : True})
+Maps.append({'name' : 'CompensationCase' , 'begin' : 69, 'end' : 69, 'expand' : True})
+Maps.append({'name' : 'OccupationOfOffender' , 'begin' : 70, 'end' : 72, 'expand' : True})
