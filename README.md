df5fb03fd1e1ac1c937ae570b19424a6b31b0511
Chris Wilson <christopher.e.wilson@gmail.com>
2014-05-19 10:37:05 -0400

    Allow for filtering down to state-level executions and aggregate data

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 death-penalty-data
 ==================
 
-Data and visualization of all capital punishments in the U.S., 1608-present
+Convert the Espy file into clean CSV or JSON documents
+
+##Data
+Data for U.S. executions from 1608 through 2002 is provided by the ["ESPY file"](http://www.icpsr.umich.edu/icpsrweb/NACJD/studies/8451?archive=NACJD&q=espy&searchSource=revise), which is freely available for download from the Interuniversity Consortium for Political and Social Research. The license for this data prevents TIME from distributing the file in this repo, so you'll need to download it yourself into this directory. The file is named `08451-0001-Data.txt`.
+
+##Running
+After downloading the data, convert to 
--- main.py
+++ main.py
@@ -1,27 +1,43 @@
+#!/usr/bin/env python
+
+import sys
+import os.path
 import ColumnMap
 import Mapping
 import Execution
-import pprint
-import json
-import csv
+from collections import defaultdict
+
+import pprint, json, csv
 
 def prettyprint(AllExecutions):
 	pprint.pprint(AllExecutions)
 
-def wrteJSON(AllExecutions):
-	with open("data-json.json", "w") as outfile:
+def writeJSON(AllExecutions):
+	with open("data.json", "w") as outfile:
 	    json.dump(AllExecutions, outfile, indent=4)
 
 def writeCSV(AllExecutions):
-	spamWriter = csv.writer(open('data.csv', 'wb'), delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+	spamWriter = csv.writer(open('espy.csv', 'wb'), delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 	spamWriter.writerow(AllExecutions[1].keys())
 	for execution in AllExecutions:
 		spamWriter.writerow(execution.values())
+
+def writeCSVList(AllExecutions):
+	spamWriter = csv.writer(open('data.csv', 'wb'), delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+	#spamWriter.writerow(AllExecutions[1].keys())
+	for execution in AllExecutions:
+		spamWriter.writerow(execution)
+
 
 Maps = ColumnMap.Maps
 CrimeMappings = Mapping.CrimeMappings
 AllExecutions = []
 
+if not os.path.isfile('08451-0001-Data.txt'):
+	print "Couldn't find '08451-0001-Data.txt'. You need to download it manually (see README)"
+	sys.exit()
+
+#1608-2002
 with open('08451-0001-Data.txt') as asciidata:
  for penalty in asciidata:
  	current_execution = Execution.Execution()
@@ -43,3 +59,27 @@ with open('08451-0001-Data.txt') as asciidata:
 	AllExecutions.append(current_execution.getExecution())
 
 writeCSV(AllExecutions)
+
+print "found %d total executions in the data" % len(AllExecutions)
+
+if "--states" in sys.argv:
+	AllExecutions = [ex for ex in AllExecutions if ex["JurisdictionOfExecution"] == "State"]	
+	print "reduced to %d executions carried out by states" % len(AllExecutions)
+
+data = defaultdict(lambda: defaultdict(int))
+total = defaultdict(int)
+
+for ex in AllExecutions:
+	data[ex['DateYear']][ex['StateOfExecution']] += 1
+	total[ex['StateOfExecution']] += 1
+
+headers = sorted(total.keys())
+output = [["year"] + headers]
+
+for y in range(1700, 2003):
+	datum = [y]
+	for state in headers:		
+		datum.append(0 if state not in data[str(y)] else data[str(y)][state])
+	output.append(datum)
+
+writeCSVList(output)
